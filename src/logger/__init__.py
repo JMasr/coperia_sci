8729dafbc95bbd96e870479cb22814235c243507
Jose Manuel R <moscaelectronica@gmail>
2024-05-09 12:52:47 +0200

    Minor naming refactoring.

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -3,5 +3,5 @@ import os
 from src.logger.basic_logger import BasicLogger
 
 app_logger = BasicLogger(
-    log_file=os.path.join(os.getcwd(), "logs", "backend.log")
+    log_file=os.path.join(os.getcwd(), "logs", "experiment.log")
 ).get_logger()
