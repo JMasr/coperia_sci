f95c34a4091d2f27e2dec4baf03a0cc6e8c515f0
José Manuel R <moscaelectronica@gmail>
2024-05-09 17:00:51 +0200

    Minor refactoring for a better code: 1.Better logger policies 2.Better exceptions

--- src/dataset/basic_dataset.py
+++ src/dataset/basic_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import KFold, train_test_split
 
 from src.logger import app_logger
-from src.files import csv_file_to_dataframe
+from src.files import csv_file_to_dataframe, json_file_to_dict
 from src.exceptions import MetadataError
 
 
@@ -14,12 +14,15 @@ class LocalDataset:
             raise TypeError("The name of the dataset must be a string")
 
         self.name: str = name
-        self.raw_metadata: pd.DataFrame = pd.DataFrame()
+        self.config: dict = {}
 
+        self.raw_metadata: pd.DataFrame = pd.DataFrame()
         self.post_processed_metadata: pd.DataFrame = pd.DataFrame()
-        self.subsets: list = []
 
         app_logger.info(f"LocalDataset - Successful created with name: {name}")
+
+    def load_config_from_json(self, str_with_path: str):
+        self.config = json_file_to_dict(str_with_path)
 
     def load_metadata_from_csv(self,
                                path_to_metadata: str,
@@ -27,8 +30,11 @@ class LocalDataset:
         try:
             self.raw_metadata = csv_file_to_dataframe(path_to_metadata, **kwargs)
             app_logger.info("LocalDataset - The CSV file was successful read")
-        except ValueError or FileNotFoundError as e:
-            app_logger.error(f"LocalDataset - The CSV file was´t read successful: {e}")
+        except ValueError as e:
+            app_logger.error(f"LocalDataset - Pandas failed reading the CSV: {e}")
+            raise MetadataError(f"{e}")
+        except FileNotFoundError as e:
+            app_logger.error(f"LocalDataset - The file wasn´t found: {e}")
             raise MetadataError(f"{e}")
 
     def transform_metadata(self,
@@ -37,13 +43,17 @@ class LocalDataset:
             message = "LocalDataset - Metadata is empty. Please, load a metadata first."
             app_logger.error(message)
             raise ValueError(message)
+        if not transformations:
+            message = "LocalDataset - No transformations were provided."
+            app_logger.error(message)
+            raise ValueError(message)
         else:
             try:
                 app_logger.info(f"LocalDataset - Starting {len(transformations)} transformations over metadata: ")
-                df = self.raw_metadata.copy()
+                df = self.raw_metadata.copy(deep=False)
                 for index, transformation in enumerate(transformations):
                     df = transformation(df)
-                    app_logger.info(f"LocalDataset - Transformation #{index +1} finished successfully")
+                    app_logger.debug(f"LocalDataset - Transformation #{index +1} finished successfully")
 
                 self.post_processed_metadata = df
             except Exception as e:
@@ -70,7 +80,7 @@ class LocalDataset:
 
         # Prepare the metadata
         try:
-            exp_metadata = self.post_processed_metadata.copy()
+            exp_metadata = self.post_processed_metadata.copy(deep=False)
             target_class_data = exp_metadata[[target_class_for_fold, target_label_for_fold]].drop_duplicates()
             target_ids = target_class_data[target_class_for_fold]
             labels_of_class = target_class_data[target_label_for_fold]
@@ -92,9 +102,10 @@ class LocalDataset:
             labels_test = sample_data_test[target_label_for_fold]
 
             # Log the lengths of the subsets
-            app_logger.info("LocalDataset - Subsets creation successful")
-            app_logger.info(f"LocalDataset - Test-set: {len(pat_test)} instance & {len(sample_data_test)} samples")
-            app_logger.info(f"LocalDataset - Train-set: {len(pat_train):} instance & {len(sample_data_train)} samples")
+            app_logger.debug("LocalDataset - Subsets creation successful")
+            app_logger.debug(f"LocalDataset - Test-set: {len(pat_test)} instance & {len(sample_data_test)} samples")
+            app_logger.debug(f"LocalDataset - Train-set: {len(pat_train):} instance & {len(sample_data_train)} samples")
+
             return [[samples_train, samples_test, labels_train, labels_test]]
         except Exception as e:
             app_logger.error(f"LocalDataset - The subsets creation fails. Error: {e}")
@@ -109,7 +120,7 @@ class LocalDataset:
         app_logger.info(f"LocalDataset - Starting the creation of {k_fold}-folds for train and test")
 
         try:
-            exp_metadata = self.post_processed_metadata.copy()
+            exp_metadata = self.post_processed_metadata.copy(deep=False)
 
             # Create the KFold object
             k_folds = []
@@ -127,7 +138,6 @@ class LocalDataset:
                 k_folds.append([samples_train, samples_test, lable_train, lable_test])
 
             app_logger.info("LocalDataset - K-folds process successful")
-            self.subsets = k_folds
             return k_folds
         except Exception as e:
             app_logger.error(f"LocalDataset - K-folds process fails. Error: {e}")
