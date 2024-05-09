6a1eb22bb184e4c12ebaeb298999db787d5b54b3
José Manuel R <moscaelectronica@gmail>
2024-05-09 16:58:22 +0200

    Mayor refactoring: 1. Encapsulation of all test for class LocalDataset

--- test/test_for_dataset.py
+++ test/test_for_dataset.py
@@ -1,75 +1,78 @@
 import pytest
 import pandas as pd
-
 from pathlib import Path
-
 from src.dataset.basic_dataset import LocalDataset
 
 
-def test_load_metadata_of_dataset_from_a_csv():
-    # Arrange
-    temp_file = Path("temp.csv")
-    temp_file.write_text('key,value\n1,"2,017"', encoding="utf-8")
+class TestLocalDataset:
+    @classmethod
+    def setup_class(cls):
+        # Create a temporary CSV file for testing
+        cls.str_path_temp_file = "temp.csv"
+        cls.temp_file = Path(cls.str_path_temp_file)
+        cls.temp_file.write_text('key,value\n1,"2,0"\n2,4\n3,8', encoding="utf-8")
 
-    # Act
-    dataset = LocalDataset(name="TEST-DATASET")
-    dataset.load_metadata_from_csv("temp.csv", decimal=",")
+    @classmethod
+    def teardown_class(cls):
+        # Remove the temporary CSV file after testing
+        cls.temp_file.unlink()
 
-    # Assert
-    assert isinstance(dataset.raw_metadata, pd.DataFrame)
+    def test_load_metadata_of_dataset_from_a_csv(self):
+        # Arrange
+        dataset = LocalDataset(name="TEST-DATASET")
 
-    # Cleanup
-    temp_file.unlink()
+        # Act
+        dataset.load_metadata_from_csv(self.str_path_temp_file, decimal=",")
 
+        # Assert
+        assert isinstance(dataset.raw_metadata, pd.DataFrame)
 
-def test_valid_initialization_of_localdataset():
-    # Arrange
-    name = "TEST-DATASET"
+    def test_valid_initialization_of_localdataset(self):
+        # Arrange
+        name = "TEST-DATASET"
 
-    # Act
-    dataset = LocalDataset(name)
-
-    # Assert
-    assert dataset.name == name
-    assert dataset.raw_metadata.empty
-    assert dataset.post_processed_metadata.empty
-    assert dataset.subsets == []
-
-
-def test_invalid_initialization_of_localdataset():
-    # Arrange
-    name = 123
-
-    # Act & Assert
-    with pytest.raises(TypeError):
+        # Act
         dataset = LocalDataset(name)
 
+        # Assert
+        assert dataset.name == name
+        assert dataset.raw_metadata.empty
+        assert dataset.post_processed_metadata.empty
+        assert dataset.subsets == []
 
-def test_valid_transformations_over_metadata_in_a_localdataset():
-    # Arrange
-    temp_file = Path("temp.csv")
-    temp_file.write_text('key,value\n1,"2,0"\n2,4\n3,8', encoding="utf-8")
+    def test_invalid_initialization_of_localdataset(self):
+        # Arrange
+        name = 123
 
-    dataset = LocalDataset(name="TEST-DATASET")
-    dataset.load_metadata_from_csv("temp.csv", decimal=",")
+        # Act & Assert
+        with pytest.raises(TypeError):
+            dataset = LocalDataset(name)
 
-    def transformation(df: pd.DataFrame) -> pd.DataFrame:
-        # Multiply the all values by 2
-        df["value"] = df["value"].astype(int) * 2
-        return df
+    def test_valid_transformations_over_metadata_in_a_localdataset(self):
+        # Arrange
+        dataset = LocalDataset(name="TEST-DATASET")
+        dataset.load_metadata_from_csv(self.str_path_temp_file, decimal=",")
 
-    # Act
-    dataset.transform_metadata([transformation])
+        def transformation(df: pd.DataFrame) -> pd.DataFrame:
+            # Multiply all values by 2
+            df["value"] = df["value"].astype(int) * 2
+            return df
 
-    # Assert
-    assert not dataset.post_processed_metadata.empty
-    # Check the transformation. The value of each position should be multiplied by 2
-    assert dataset.post_processed_metadata["value"].tolist() == [4, 8, 16]
-    assert dataset.raw_metadata["value"].tolist() == [2.0, 4, 8]
+        # Act
+        dataset.transform_metadata([transformation])
 
-    # Cleanup
-    temp_file.unlink()
+        # Assert
+        assert not dataset.post_processed_metadata.empty
+        assert dataset.post_processed_metadata["value"].tolist() == [4, 8, 16]
+        assert dataset.raw_metadata["value"].tolist() == [2.0, 4, 8]
 
+    def test_invalid_empty_transformations_provided_for_localdataset(self):
+        # Arrange
+        dataset = LocalDataset(name="TEST-DATASET")
+
+        # Act & Assert
+        with pytest.raises(ValueError):
+            dataset.transform_metadata([])
 
 # TODO: The class LocalDataset isn´t fully tested
 
