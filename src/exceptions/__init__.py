f1aef78b06d62c4cc145144941c73a578ca10288
José Manuel R. <moscaelectronica@gmail.com>
2024-05-28 10:36:44 +0200

    Adding a generic exception for audio processing errors

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -3,3 +3,10 @@ class MetadataError(Exception):
     Exception raised when an error occurs while transforming pd.DataFrames
     """
     pass
+
+
+class AudioProcessingError(Exception):
+    """
+    Exception raised when an error occurs while processing audio files
+    """
+    pass
