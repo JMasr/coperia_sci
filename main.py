13cd56bd0018a2f70ab3435cdc62e76d865c00fd
José Manuel R. <moscaelectronica@gmail.com>
2024-05-28 18:35:14 +0200

    Mayor refactoring: 1. New pipelines with save and load 2. New phase to extrac all the feats
    Signed-off-by: José Manuel R. <moscaelectronica@gmail.com>

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from src.files import json_file_to_dict
 from src.logger import app_logger
 
 
-def make_dicoperia_metadata(metadata: pd.DataFrame,
-                            filters_: dict = None,
-                            remove_samples: dict = None):
+def make_dicoperia_metadata(
+        metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
+):
     """
     Make a metadata file for the COPERIA dataset filtering some columns
     :param metadata: a list with all the audio samples in COPERIA as an Audio class
@@ -21,23 +21,29 @@ def make_dicoperia_metadata(metadata: pd.DataFrame,
     df = metadata.copy()
 
     if filters_ is None:
-        filters_ = {'patient_type': ['covid-control', 'covid-persistente']}
+        filters_ = {"patient_type": ["covid-control", "covid-persistente"]}
 
     if remove_samples is None:
         remove_samples = {
-            'audio_id': ['c15e54fc-5290-4652-a3f7-ff3b779bd980', '244b61cc-4fd7-4073-b0d8-7bacd42f6202'],
-            'patient_id': ['coperia-rehab']}
+            "audio_id": [
+                "c15e54fc-5290-4652-a3f7-ff3b779bd980",
+                "244b61cc-4fd7-4073-b0d8-7bacd42f6202",
+            ],
+            "patient_id": ["coperia-rehab"],
+        }
 
     for key, values in remove_samples.items():
         df = df[~df[key].isin(values)]
 
     for key, values in filters_.items():
-        if 'ALL' in values:
+        if "ALL" in values:
             values = list(df[key].unique())
 
         df = df[df[key].isin(values)]
 
-    df['patient_type'] = df['patient_type'].map({'covid-control': 0, 'covid-persistente': 1}).astype(int)
+    df["patient_type"] = (
+        df["patient_type"].map({"covid-control": 0, "covid-persistente": 1}).astype(int)
+    )
     return df
 
 
@@ -51,8 +57,6 @@ if __name__ == "__main__":
     config = json_file_to_dict(config_file)
 
     config_audio = config.get("audio")
-    dataset = AudioDataset(name="COPERIA-DATASET",
-                           config_audio=config_audio)
 
     config_run_experiment = config.get("run")
     test_size = config.get("run").get("test_size")
@@ -65,23 +69,31 @@ if __name__ == "__main__":
     target_data = config_dataset_experiment.get("target_data")
     target_label = config_dataset_experiment.get("target_label")
     metadata_path = config_dataset_experiment.get("path_to_csv")
+    object_path = config_dataset_experiment.get("path_to_object")
 
-    dataset.load_metadata_from_csv(metadata_path, decimal=",")
-    dataset.transform_metadata([make_dicoperia_metadata])
-    dataset.transform_column_id_2_data_path(column_name="audio_id",
-                                            path=dataset_raw_data_path,
-                                            extension=".wav")
+    if object_path:
+        dataset = AudioDataset(name="COPERIA_DATASET",
+                               storage_path=os.path.join(ROOT_PATH, "data"),
+                               config_audio=config_audio).load_dataset_from_a_serialized_object(object_path)
+        dataset.save_dataset_as_a_serialized_object()
+    else:
+        dataset = AudioDataset(name="COPERIA-DATASET", config_audio=config_audio)
+        dataset.load_metadata_from_csv(metadata_path, decimal=",")
+        dataset.transform_metadata([make_dicoperia_metadata])
+        dataset.transform_column_id_2_data_path(
+            column_name="audio_id", path=dataset_raw_data_path, extension=".wav"
+        )
+        app_logger.info("Pipeline - Making the subsets")
 
-    app_logger.info("Pipeline - Making the subsets")
+        dataset_ready_with_1_fold = dataset.make_1_fold_subsets(
+            target_class_for_fold=target_class,
+            target_label_for_fold=target_label,
+            test_size=test_size,
+            seed=seed,
+        )
+        dataset_ready_with_3_fold = dataset.make_k_fold_subsets(
+            target_class_for_fold=target_class, k_fold=k_fold, seed=seed
+        )
 
-    dataset_ready_with_1_fold = dataset.make_1_fold_subsets(target_class_for_fold=target_class,
-                                                            target_label_for_fold=target_label,
-                                                            test_size=test_size,
-                                                            seed=seed)
-    dataset_ready_with_3_fold = dataset.make_k_fold_subsets(target_class_for_fold=target_class,
-                                                            k_fold=k_fold,
-                                                            seed=seed)
-
-    dataset.load_raw_data_using_a_path()
-    dataset.extract_acoustic_features()
-    app_logger.info("Saving the subsets...")
+        dataset.load_raw_data_using_a_path()
+        dataset.save_dataset_as_a_serialized_object()
