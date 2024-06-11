7ce7fe425f7ca02e921ad861ddbd1c178e9fd40c
Jose M. Ramirez <moscaelectronica@gmail.com>
2024-06-11 12:01:27 +0200

    refactor(Model Module): Adding a specific exceprion for ModelBuilder
    BREAKING CHANGE: no

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -14,6 +14,14 @@ class AudioProcessingError(Exception):
     pass
 
 
+class ModelError(Exception):
+    """
+    Exception raised when an error occurs while processing audio files
+    """
+
+    pass
+
+
 class ExperimentError(Exception):
     """
     Exception raised when an error occurs while running an experiment
--- src/model/model_object.py
+++ src/model/model_object.py
@@ -8,55 +8,62 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
+from exceptions import ModelError
 from src.logger import app_logger
 
-DEFAULT_CONFIG = types.MappingProxyType({
-    "LogisticRegression": {
-        "C": 0.01,
-        "max_iter": 40,
-        "penalty": "l2",
-        "random_state": 42,
-        "solver": "liblinear",
-        "class_weight": "balanced",
-        "verbose": True,
-    },
-    "RandomForest": {
-        "n_estimators": 20,
-        "criterion": "gini",
-        "max_depth": None,
-        "min_samples_split": 2,
-        "min_samples_leaf": 1,
-        "max_features": "sqrt",
-        "class_weight": "balanced",
-        "random_state": 42,
-        "verbose": True,
-    },
-    "LinearSVM": {
-        "C": 0.1,
-        "tol": 0.001,
-        "max_iter": 1000,
-        "class_weight": None,
-        "verbose": True,
-        "random_state": 42,
-    },
-    "MLP": {
-        "alpha": 0.001,
-        "max_iter": 1500,
-        "solver": "adam",
-        "activation": "tanh",
-        "learning_rate_init": 0.001,
-        "hidden_layer_sizes": [20, 20],
-        "verbose": True,
-        "random_state": 42,
-    },
-})
+DEFAULT_CONFIG = types.MappingProxyType(
+    {
+        "LogisticRegression": {
+            "C": 0.01,
+            "max_iter": 40,
+            "penalty": "l2",
+            "random_state": 42,
+            "solver": "liblinear",
+            "class_weight": "balanced",
+            "verbose": True,
+        },
+        "RandomForest": {
+            "n_estimators": 20,
+            "criterion": "gini",
+            "max_depth": None,
+            "min_samples_split": 2,
+            "min_samples_leaf": 1,
+            "max_features": "sqrt",
+            "class_weight": "balanced",
+            "random_state": 42,
+            "verbose": True,
+        },
+        "LinearSVM": {
+            "C": 0.1,
+            "tol": 0.001,
+            "max_iter": 1000,
+            "class_weight": None,
+            "verbose": True,
+            "random_state": 42,
+        },
+        "MLP": {
+            "alpha": 0.001,
+            "max_iter": 1500,
+            "solver": "adam",
+            "activation": "tanh",
+            "learning_rate_init": 0.001,
+            "hidden_layer_sizes": [20, 20],
+            "verbose": True,
+            "random_state": 42,
+        },
+    }
+)
 
-SUPPORTED_MODELS = types.MappingProxyType({
-    "LogisticRegression": LogisticRegression(**DEFAULT_CONFIG["LogisticRegression"]),
-    "RandomForest": RandomForestClassifier(**DEFAULT_CONFIG["RandomForest"]),
-    "LinearSVM": SVC(**DEFAULT_CONFIG["LinearSVM"]),
-    "MLP": MLPClassifier(**DEFAULT_CONFIG["MLP"]),
-})
+SUPPORTED_MODELS = types.MappingProxyType(
+    {
+        "LogisticRegression": LogisticRegression(
+            **DEFAULT_CONFIG["LogisticRegression"]
+        ),
+        "RandomForest": RandomForestClassifier(**DEFAULT_CONFIG["RandomForest"]),
+        "LinearSVM": SVC(**DEFAULT_CONFIG["LinearSVM"]),
+        "MLP": MLPClassifier(**DEFAULT_CONFIG["MLP"]),
+    }
+)
 
 
 class ModelBuilder(BaseModel):
@@ -76,15 +83,19 @@ class ModelBuilder(BaseModel):
             # Check that all parameters in the default configuration are present
             for key in DEFAULT_CONFIG[self.name].keys():
                 if key not in self.parameters.keys():
-                    app_logger.warning(f"Parameter {key} not found in the model configuration."
-                                       f" Using default value: {DEFAULT_CONFIG[self.name][key]}")
+                    app_logger.warning(
+                        f"Parameter {key} not found in the model configuration."
+                        f" Using default value: {DEFAULT_CONFIG[self.name][key]}"
+                    )
                     self.parameters[key] = DEFAULT_CONFIG[self.name][key]
 
             # Check that all parameters in the model configuration are valid
             for key in self.parameters.keys():
                 if key not in DEFAULT_CONFIG[self.name].keys():
-                    app_logger.warning(f"Parameter {key} not found in the default configuration."
-                                       f" Removing parameter from the model configuration.")
+                    app_logger.warning(
+                        f"Parameter {key} not found in the default configuration."
+                        f" Removing parameter from the model configuration."
+                    )
                     self.parameters.pop(key)
 
     def save_as_a_serialized_object(self, path_to_save: str = None):
@@ -132,27 +143,32 @@ class ModelBuilder(BaseModel):
                 model = SUPPORTED_MODELS[self.name]
                 model.set_params(**self.parameters)
             else:
-                app_logger.warning(f"No parameters found for the model. Using default configuration.")
+                app_logger.warning(
+                    f"No parameters found for the model. Using default configuration."
+                )
                 model = SUPPORTED_MODELS[self.name]
         except Exception as e:
             message = f"ModelBuilder - An error occurred while loading the model: {e}"
             app_logger.error(message)
-            raise Exception(message)
+            raise ModelError(message)
 
         self.model = model
         app_logger.info(f"ModelBuilder - {self.name} build successfully.")
 
     def train_model(self, x, y):
         if self.model is None:
-            raise ValueError("Model is not built yet. Please build the model first.")
+            raise ValueError(
+                "ModelBuilder - Model is not built yet. Please build the model first."
+            )
 
         try:
             self.model.fit(x, y)
             self.is_trained = True
-            app_logger.info(f"Model trained successfully.")
+            app_logger.info(f"ModelBuilder - Model trained successfully.")
         except Exception as e:
-            message = f"An error occurred while training the model: {e}"
-            app_logger.error(message)
-            raise Exception(message)
+            app_logger.error(
+                f"ModelBuilder - An error occurred while training the model: {e}"
+            )
+            raise ModelError(e)
 
         return self.model
