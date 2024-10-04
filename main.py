b69bc11f2061ace952bbc8917acc8626bd7abb9c
Jose M. Ramirez <moscaelectronica@gmail.com>
2024-10-04 11:42:27 +0200

    feat(MAIN): Add new function to filter data on the dataset & all the supported models on the exp pipeline

--- main.py
+++ main.py
@@ -180,6 +180,95 @@ def make_dicoperia_metadata_cougth(
     return df
 
 
+def make_dicoperia_metadata_cougth_after(
+        metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
+):
+    """
+    Make a metadata file for the COPERIA dataset filtering some columns
+    :param metadata: a list with all the audio samples in COPERIA as an Audio class
+    :param filters_: a dictionary with the columns and values to keep
+    :param remove_samples: a dictionary with the columns and values to remove
+    :return: a pandas dataframe with the metadata of the DICOPERIA dataset
+    """
+    df = metadata.copy()
+
+    if filters_ is None:
+        filters_ = {
+            "patient_type": ["covid-control", "covid-persistente"],
+            "audio_type": ["/cough/"],
+            "audio_moment": ["after"],
+        }
+
+    if remove_samples is None:
+        remove_samples = {
+            "audio_id": [
+                "c15e54fc-5290-4652-a3f7-ff3b779bd980",
+                "244b61cc-4fd7-4073-b0d8-7bacd42f6202",
+            ],
+            "patient_id": ["coperia-rehab"],
+        }
+
+    for key, values in remove_samples.items():
+        df = df[~df[key].isin(values)]
+
+    for key, values in filters_.items():
+        if "ALL" in values:
+            values = list(df[key].unique())
+
+        df = df[df[key].isin(values)]
+
+    df["patient_type"] = (
+        df["patient_type"]
+        .map({"covid-control": True, "covid-persistente": False})
+        .astype(bool)
+    )
+    return df
+
+
+def make_dicoperia_metadata_cougth_before(
+        metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
+):
+    """
+    Make a metadata file for the COPERIA dataset filtering some columns
+    :param metadata: a list with all the audio samples in COPERIA as an Audio class
+    :param filters_: a dictionary with the columns and values to keep
+    :param remove_samples: a dictionary with the columns and values to remove
+    :return: a pandas dataframe with the metadata of the DICOPERIA dataset
+    """
+    df = metadata.copy()
+
+    if filters_ is None:
+        filters_ = {
+            "patient_type": ["covid-control", "covid-persistente"],
+            "audio_type": ["/cough/"],
+            "audio_moment": ["before"],
+        }
+
+    if remove_samples is None:
+        remove_samples = {
+            "audio_id": [
+                "c15e54fc-5290-4652-a3f7-ff3b779bd980",
+                "244b61cc-4fd7-4073-b0d8-7bacd42f6202",
+            ],
+            "patient_id": ["coperia-rehab"],
+        }
+
+    for key, values in remove_samples.items():
+        df = df[~df[key].isin(values)]
+
+    for key, values in filters_.items():
+        if "ALL" in values:
+            values = list(df[key].unique())
+
+        df = df[df[key].isin(values)]
+
+    df["patient_type"] = (
+        df["patient_type"]
+        .map({"covid-control": True, "covid-persistente": False})
+        .astype(bool)
+    )
+    return df
+
 def make_dicoperia_metadata_a(
         metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
 ):
@@ -224,6 +313,95 @@ def make_dicoperia_metadata_a(
     return df
 
 
+def make_dicoperia_metadata_a_after(
+        metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
+):
+    """
+    Make a metadata file for the COPERIA dataset filtering some columns
+    :param metadata: a list with all the audio samples in COPERIA as an Audio class
+    :param filters_: a dictionary with the columns and values to keep
+    :param remove_samples: a dictionary with the columns and values to remove
+    :return: a pandas dataframe with the metadata of the DICOPERIA dataset
+    """
+    df = metadata.copy()
+
+    if filters_ is None:
+        filters_ = {
+            "patient_type": ["covid-control", "covid-persistente"],
+            "audio_type": ["/a/"],
+            "audio_moment": ["after"],
+        }
+
+    if remove_samples is None:
+        remove_samples = {
+            "audio_id": [
+                "c15e54fc-5290-4652-a3f7-ff3b779bd980",
+                "244b61cc-4fd7-4073-b0d8-7bacd42f6202",
+            ],
+            "patient_id": ["coperia-rehab"],
+        }
+
+    for key, values in remove_samples.items():
+        df = df[~df[key].isin(values)]
+
+    for key, values in filters_.items():
+        if "ALL" in values:
+            values = list(df[key].unique())
+
+        df = df[df[key].isin(values)]
+
+    df["patient_type"] = (
+        df["patient_type"]
+        .map({"covid-control": True, "covid-persistente": False})
+        .astype(bool)
+    )
+    return df
+
+
+def make_dicoperia_metadata_a_before(
+        metadata: pd.DataFrame, filters_: dict = None, remove_samples: dict = None
+):
+    """
+    Make a metadata file for the COPERIA dataset filtering some columns
+    :param metadata: a list with all the audio samples in COPERIA as an Audio class
+    :param filters_: a dictionary with the columns and values to keep
+    :param remove_samples: a dictionary with the columns and values to remove
+    :return: a pandas dataframe with the metadata of the DICOPERIA dataset
+    """
+    df = metadata.copy()
+
+    if filters_ is None:
+        filters_ = {
+            "patient_type": ["covid-control", "covid-persistente"],
+            "audio_type": ["/a/"],
+            "audio_moment": ["before"],
+        }
+
+    if remove_samples is None:
+        remove_samples = {
+            "audio_id": [
+                "c15e54fc-5290-4652-a3f7-ff3b779bd980",
+                "244b61cc-4fd7-4073-b0d8-7bacd42f6202",
+            ],
+            "patient_id": ["coperia-rehab"],
+        }
+
+    for key, values in remove_samples.items():
+        df = df[~df[key].isin(values)]
+
+    for key, values in filters_.items():
+        if "ALL" in values:
+            values = list(df[key].unique())
+
+        df = df[df[key].isin(values)]
+
+    df["patient_type"] = (
+        df["patient_type"]
+        .map({"covid-control": True, "covid-persistente": False})
+        .astype(bool)
+    )
+    return df
+
 if __name__ == "__main__":
     ROOT_PATH = Path(__file__).parent
 
@@ -242,49 +420,97 @@ if __name__ == "__main__":
         root_path=ROOT_PATH,
         config_file_path=config_file,
     )
+    pipeline_app.setup_using_config()
 
-    # Run the pipeline with all the data and default configuration
-    pipeline_app.run_pipeline_with_an_experiment_from_config(make_dicoperia_metadata)
+    for model in ["LogisticRegression", "RandomForest", "MLP"]:
+        pipeline_app.configurations_as_dict["model"]["name"] = model
 
-    # Run the pipeline with all the data after and default configuration
-    new_pipeline_config = pipeline_app.configurations_as_dict
-    new_pipeline_config["run"]["run_name"] = "COPERIA-AFTER"
-    new_pipeline_config["dataset"]["path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-AFTER.pkl"
-    new_pipeline_config["dataset"]["filters"] = {"audio_type": ["ALL"], "audio_moment": ["after"]}
-    pipeline_app.configurations_as_dict = new_pipeline_config
-    pipeline_app.run_pipeline_with_an_experiment_from_config(
-        make_dicoperia_metadata_after
-    )
+        # Run the pipeline with all the data and default configuration
+        pipeline_app.run_pipeline_with_an_experiment_from_config(make_dicoperia_metadata)
 
-    # Run the pipeline with all the data before and default configuration
-    new_pipeline_config = pipeline_app.configurations_as_dict
-    new_pipeline_config["run"]["run_name"] = "COPERIA-BEFORE"
-    new_pipeline_config["dataset"]["name"] = "COPERIA-BEFORE"
-    new_pipeline_config["dataset"]["path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-BEFORE.pkl"
-    new_pipeline_config["dataset"]["filters"] = {"audio_type": ["ALL"], "audio_moment": ["before"]}
-    pipeline_app.configurations_as_dict = new_pipeline_config
-    pipeline_app.run_pipeline_with_an_experiment_from_config(
-        make_dicoperia_metadata_before
-    )
+        # Run the pipeline with all the data after and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-AFTER"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-AFTER.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["ALL"], "audio_moment": ["after"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_after
+        )
 
-    # Run the pipeline with all the data cough and default configuration
-    new_pipeline_config = pipeline_app.configurations_as_dict
-    new_pipeline_config["run"]["run_name"] = "COPERIA-COUGH"
-    new_pipeline_config["dataset"]["name"] = "COPERIA-COUGH"
-    new_pipeline_config["dataset"]["path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-COUGH.pkl"
-    new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/cough/"], "audio_moment": ["ALL"]}
-    pipeline_app.configurations_as_dict = new_pipeline_config
-    pipeline_app.run_pipeline_with_an_experiment_from_config(
-        make_dicoperia_metadata_cougth
-    )
+        # Run the pipeline with all the data before and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-BEFORE"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-BEFORE.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["ALL"], "audio_moment": ["before"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_before
+        )
 
-    # Run the pipeline with all the data a and default configuration
-    new_pipeline_config = pipeline_app.configurations_as_dict
-    new_pipeline_config["run"]["run_name"] = "COPERIA-A"
-    new_pipeline_config["dataset"]["name"] = "COPERIA-A"
-    new_pipeline_config["dataset"]["path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-A.pkl"
-    new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/a/"], "audio_moment": ["ALL"]}
-    pipeline_app.configurations_as_dict = new_pipeline_config
-    pipeline_app.run_pipeline_with_an_experiment_from_config(
-        make_dicoperia_metadata_a
-    )
+        # Run the pipeline with all the data cough and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-COUGH"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-COUGH.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/cough/"], "audio_moment": ["ALL"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_cougth
+        )
+
+        # Run the pipeline with all the data a and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-A"
+        new_pipeline_config["dataset"]["path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-A.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/a/"], "audio_moment": ["ALL"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_a
+        )
+
+        # Run the pipeline with all the data a and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-A-AFTER"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-A-AFTER.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/a/"], "audio_moment": ["after"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_a_after
+        )
+
+        # Run the pipeline with all the data a and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-A-BEFORE"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-A-BEFORE.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/a/"], "audio_moment": ["before"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_a_before
+        )
+
+        # Run the pipeline with all the data a and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-COUGH-AFTER"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-COUGH-AFTER.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/cough/"], "audio_moment": ["after"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_cougth_after
+        )
+
+        # Run the pipeline with all the data a and default configuration
+        new_pipeline_config = pipeline_app.configurations_as_dict
+        new_pipeline_config["run"]["run_name"] = "COPERIA-COUGH-BEFORE"
+        new_pipeline_config["dataset"][
+            "path_to_object"] = "/home/visia/Documents/GitHub/coperia_sci/exp/COPERIA-COUGH-BEFORE.pkl"
+        new_pipeline_config["dataset"]["filters"] = {"audio_type": ["/cough/"], "audio_moment": ["before"]}
+        pipeline_app.configurations_as_dict = new_pipeline_config
+        pipeline_app.run_pipeline_with_an_experiment_from_config(
+            make_dicoperia_metadata_cougth_before
+        )
